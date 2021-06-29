f013196ef76154715499d3b3badf449e20de85fd
GabrielaMarinova <Gabriela.Marinova@stud.uni-heidelberg.de>
2021-06-30 00:48:43 +0200

    Organisation

--- Classes/knn.py
+++ Classes/knn.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # trainimages_pca: transformed training images using pca
 # new_image: tasted data image, array, after pca
 
@@ -9,12 +10,10 @@ def euclidean_distance(trainimages_pca, new_image):
 
 # trainlabels = string with the names of pictures, vector
 # k = number of neighbors
-def KNN(trainimages_pca, trainlabels, new_image, k):
+
+
+def Knn(trainimages_pca, trainlabels, new_image, k):
     for train_image in trainimages_pca:
         dist = euclidean_distance(train_image, new_image)
-        neighbors= np.sort(dist)[:k] # sorting the array, first element is the smallest
-        return neighbors
-
-
-
-
+        neighbors = np.sort(dist)[:k]  # sorting the array, first element is the smallest
+    return neighbors
