46d60afba31c90929fa513677bc4435946a80025
GabrielaMarinova <Gabriela.Marinova@stud.uni-heidelberg.de>
2021-06-30 00:26:35 +0200

    Meeting notes and KNN function

---
+++ knn.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# trainimages_pca: transformed training images using pca
+# new_image: tasted data image, array, after pca
+
+def euclidean_distance(trainimages_pca, new_image):
+    distance = np.sqrt(np.sum(np.square(trainimages_pca - new_image)))
+    return distance
+
+# trainlabels = string with the names of pictures, vector
+# k = number of neighbors
+def KNN(trainimages_pca, trainlabels, new_image, k):
+    for train_image in trainimages_pca:
+        dist = euclidean_distance(train_image, new_image)
+        neighbors= np.sort(dist)[:k] # sorting the array, first element is the smallest
+        return neighbors
+
+
+
+
