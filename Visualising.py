ebd1f04e9bba4b7b21d9e7dca139f616cf5532ea
georgigeorgi <georgi.georgiev@stud.uni-heidelberg.de>
2021-07-16 17:18:22 +0300

    Functions for visualisation of the data.

---
+++ Visualising.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+
+
+def train_show(no_images_training, train_matrix, image_height, image_width):
+    """ Shows a representation of the training set
+            \n
+            The no_images_training is a vector where each element shows how many pictures the person has for training .
+            \n
+            The train_matrix is the matrix of the training set.
+            \n
+            image_height, image_width describe how big the image is.
+        """
+    for i in range(0, 36):
+        first_image = i*(no_images_training[i])
+        vec = (train_matrix[first_image])
+        plt.subplot(6, 6, i + 1)
+        plt.imshow(vec.reshape(image_height, image_width), cmap="gray")
+    plt.show()
+
+
+def test_show(no_images_testing, test_matrix, image_height, image_width):
+    """ Shows a representation of the training set
+            \n
+            The no_images_testing is a vector where each element shows how many pictures the person has for testing .
+            \n
+            The test_matrix is the matrix of the testing set.
+            \n
+            image_height, image_width describe how big the image is.
+        """
+    for i in range(0, 36):
+        first_image = i*(no_images_testing[i])
+        vec = (test_matrix[first_image])
+        plt.subplot(6, 6, i + 1)
+        plt.imshow(vec.reshape(image_height, image_width), cmap="gray")
+    plt.show()
+
+
+def vector_show(vector, image_height, image_width):
+    """ Shows an image from a vector
+            \n
+            The vector is an image vector to be reshaped into an image
+            \n
+            image_height, image_width describe how big the image is.
+        """
+    plt.imshow(vector.reshape(image_height, image_width), cmap="gray")
+    plt.show()
